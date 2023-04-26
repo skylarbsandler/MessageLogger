64ab9c6cd7ff47b372b6435d9234a6bd4ff1ee52
skylarbsandler <skylarbsandler@gmail.com>
2023-04-26 07:54:34 -0600

    git

--- MessageLogger/Program.cs
+++ MessageLogger/Program.cs
@@ -3,6 +3,7 @@ using MessageLogger;
 using System;
 
 List<User> activeUsers = new List<User>();
+bool activeUser = false;
 
 Console.WriteLine("Welcome to Message Logger!");
 Console.WriteLine("");
@@ -14,6 +15,7 @@ Console.Write("What is your username? (one word, no spaces!) ");
 var userName = Console.ReadLine();
 var user1 = new User(name, userName);
 activeUsers.Add(user1);
+activeUser = true;
 
 Console.WriteLine("");
 Console.WriteLine("To log out of your user profile, enter 'log out'. To quit the application, enter 'quit'");
@@ -23,41 +25,71 @@ Console.Write("Add a message: ");
 var userMessage = new Message(Console.ReadLine(), DateTime.Now);
 user1.AddMessage(userMessage);
 
-while (userMessage.Content != "quit")
+while (true)
 {
     //only runs if first message input is "log out"
     //need to check if most recent/last message contains "log out"
+    if (userMessage.Content == "quit")
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Thanks for using Message Logger!");
+        foreach (var user in activeUsers)
+        {
+            Console.WriteLine($"{user.Name} wrote {user.Messages.Count - 1} messages.");
+        }
+        break;
+    }
+
     if (userMessage.Content == "log out")
     {
-        Console.WriteLine("Would you like to log in a 'new' or 'existing' user?");
-        var userInput = Console.ReadLine();
-
-        if (userInput == "existing")
+        activeUser = false;
         {
-            Console.Write("What is your username?");
+            Console.WriteLine("Would you like to log in a 'new' or 'existing' user?");
+            var userInput = Console.ReadLine();
+
+            if (userInput == "existing")
+            {
+                Console.Write("What is your username? ");
+                userName = Console.ReadLine();
+                bool containsuserName = activeUsers.Any(user => user.Username == userName);
+                activeUser = true;
+            }
+            if (userInput == "new")
+            {
+                Console.WriteLine("Let's create a user profile for you.");
+                Console.Write("What is your name? ");
+                name = Console.ReadLine();
+                Console.Write("What is your username? (one word, no spaces!) ");
+                userName = Console.ReadLine();
+                user1 = new User(name, userName);
+                activeUsers.Add(user1);
+                activeUser = true;
+            }
         }
     }
-    else 
+
+    if (userMessage.Content != "log out" || userMessage.Content != "quit")
     {
-        foreach (var message in user1.Messages)
-        {
-            Console.WriteLine($"{user1.Name} {message.CreatedAt.ToShortTimeString()}: {message.Content}");
-        }
-        Console.Write("Add a message: ");
-        var newMessage = new Message(Console.ReadLine(), DateTime.Now);
-        user1.AddMessage(newMessage);
-        Console.WriteLine("");
+       foreach (var message in user1.Messages)
+       {
+          Console.WriteLine($"{user1.Name} {message.CreatedAt.ToShortTimeString()}: {message.Content}");
+       }
+          Console.Write("Add a message: ");
+          var newMessage = new Message(Console.ReadLine(), DateTime.Now);
+          user1.AddMessage(newMessage);
+          Console.WriteLine("");
     }
-}
+ }
 
-//only runs if first message input is "quit"
-//need to check if most recent/last message contains "quit"
-Console.WriteLine("");
-Console.WriteLine("Thanks for using Message Logger!");
-foreach (var user in activeUsers)
-{
-  Console.WriteLine($"{user.Name} wrote {user.Messages.Count} messages.");
-}
+    //only runs if first message input is "quit"
+    //need to check if most recent/last message contains "quit"
+    //Console.WriteLine("");
+    //Console.WriteLine("Thanks for using Message Logger!");
+    //foreach (var user in activeUsers)
+    //{
+    //    Console.WriteLine($"{user.Name} wrote {user.Messages.Count} messages.");
+    //}
+
 
 
 
